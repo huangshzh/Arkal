cad3397102cbc7a912cd5364921c7df852a18e8b
huangshzh <31400418+huangshzh@users.noreply.github.com>
2018-11-13 20:31:37 +0800

    2018.11.13

--- TCP_IP/main.c
+++ TCP_IP/main.c
@@ -1,9 +1,12 @@
 //
 // Created by arthur on 18-11-11.
 //
+#include <stdio.h>
+#include <head.h>
+int test;
 
 int main() {
-    printf("hello github");
+    printf("hello github %d\n", test2);
     return 0;
 }
 
